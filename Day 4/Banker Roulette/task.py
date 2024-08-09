0af10ef78aefe1d7f4cf2d28af2143b303757f9c
Sean Hupe <seanhupe@yahoo.com>
2024-08-09 16:35:22 -0700

    Update

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,12 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# Option 1
+print(random.choice(friends))
+
+# Option 2
+random_index = random.randint(0, 4)
+print(friends[random_index])
+
+
