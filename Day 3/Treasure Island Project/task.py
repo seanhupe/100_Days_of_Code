421a852d56de4b80b260af1d161aa0f780e3c164
Sean Hupe <seanhupe@gmail.com>
2024-08-08 20:03:49 -0700

    Update

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -20,7 +20,7 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-print("Welcome to Treasure Island..")
+print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 choice1 = input('You\'re at a crossroad, where do you want to go? '
                 'Type "left" or "right".\n').lower()
