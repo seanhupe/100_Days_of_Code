e2ccea4ee6b58d61b684ca6151d4c52ad7f0d005
Sean Hupe <seanhupe@yahoo.com>
2024-08-09 16:12:03 -0700

    Update

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,17 @@
+import random
+
+# num = random.randint(1, 10)
+# print(num)
+
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+#
+# random_float = random.uniform(1, 10)
+# print(random_float)
+
+rand_number = random.randint(1, 2)
+if rand_number == 1:
+    print("Heads")
+else:
+    print("Tails")
+
