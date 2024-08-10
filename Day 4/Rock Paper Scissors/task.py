0d61dc9c10a0a9d9c70e6e16701ded1145646bcd
Sean Hupe <seanhupe@yahoo.com>
2024-08-09 17:22:40 -0700

    Update

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -9,18 +11,47 @@ rock = '''
 
 paper = '''
     _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
+---'   ____)_______
+          _________)
+          __________)
+         __________)
+---._____________)
 '''
 
 scissors = '''
     _______
----'   ____)____
-          ______)
-       __________)
+---'   ____)_______
+          _________)
+       _____________)
       (____)
 ---.__(___)
 '''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors:\n"))
+print("You Chose:")
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer Chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0:
+    print("Invalid number - you lose")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice > user_choice:
+    print("You LOSE!")
+
+elif user_choice > computer_choice:
+    print("You WIN!")
+
+elif computer_choice == user_choice:
+    print("It is a Draw - Play again!")
+
+elif computer_choice == 0 and user_choice == 2:
+    print("LOSER")
+
+
+
