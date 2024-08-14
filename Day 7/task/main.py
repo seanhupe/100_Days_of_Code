17cf434cf05d46d8dde5ff46ab1ea6578537ce85
Sean Hupe <seanhupe@yahoo.com>
2024-08-14 12:55:17 -0700

    Update

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,19 +1,27 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-guess = input("Guess a letter: ").lower()
-print(guess)
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+placeholder = ""
+word_length = len(chosen_word)
+for letter in range(word_length):
+    placeholder += "_"
+print(placeholder)
 
-# Iterate over the string
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+display = ""
+
+
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
+        display += letter
     else:
-        print("Wrong")
+        display += "_"
+
+print(display)
+
