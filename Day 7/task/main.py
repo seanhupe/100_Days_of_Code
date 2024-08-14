d49bcee8f31fe9bf6b924ad8b59192dde8c3b0ed
Sean Hupe <seanhupe@yahoo.com>
2024-08-14 13:41:55 -0700

    Update

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -62,7 +62,7 @@ while not game_over:
 
     if "_" not in display:
         game_over = True
-        print("****************************YOU WIN****************************")
+        print("*****************************YOU WIN*****************************")
 
     # TODO-2: - Update the code below to use the stages List from the file hangman_art.py
     print(stages[lives])
