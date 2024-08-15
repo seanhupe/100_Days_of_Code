1c9753a981abc852d56c05186b4903379bcf0ac8
Sean Hupe <seanhupe@yahoo.com>
2024-08-15 15:47:07 -0700

    Update

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,17 @@
+## def greet(**variable**)
+
+# def greet():
+#     print("Hello Angela")
+#     print("How do you do Jack Bauer")
+#     print("Isn't the weather nice")
+# greet()
+
+
+# Functions that allows for inputs
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Shithead")
+
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,14 @@
 # Functions with input
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
 
+def greet_with(name, location):
+    print(f"Hello {name}! What is it like in {location}?")
 
-greet_with_name("Jack Bauer")
+#greet_with("Shithead", "Kansas")
+greet_with(name = "Shithead", location = "Kansas")
