bcd7847c931b74ed1f76f0c499fe8ef06e232ae7
Sean Hupe <seanhupe@yahoo.com>
2024-08-15 17:03:03 -0700

    Update

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -8,6 +8,17 @@ shift = int(input("Type the shift number:\n"))
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
+
+# def decrypt(original_text, shift_amount):
+#     output_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) - shift_amount
+#         shifted_position %= len(alphabet)
+#         output_text += alphabet[shifted_position]
+#     print(f"Here is the encoded result: {output_text}")
+#
+# decrypt(original_text=text, shift_amount=shift)
+
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
