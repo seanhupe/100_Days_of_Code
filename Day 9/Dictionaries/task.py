0895777508f0ee4e3119c695add5cdb87f87b44e
Sean Hupe <seanhupe@yahoo.com>
2024-08-22 21:39:25 -0700

    Update

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,9 +1,24 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again.",
 }
 
-print(programming_dictionary["Bug"])
+programming_dictionary["Loop"] = "The action of doing something over and over again."
 
+
+
+empty_dictionary = {}
+
+#Wipe an existing
+
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+programming_dictionary["Bug"] = "Shove it up your ass"
+print(programming_dictionary)
+
+#Loop through a dict
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
 
