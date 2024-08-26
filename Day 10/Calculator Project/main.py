375fb29822ced1dffacca646d59b416621f75124
Sean Hupe <seanhupe@yahoo.com>
2024-08-26 14:57:04 -0700

    Update

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,26 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4, 8))
+
+num1 = input("What is the first number?: ")
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation: ")
