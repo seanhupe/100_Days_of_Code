96f9d3da94c664733dbcfcf6da614cea659537f1
Sean Hupe <seanhupe@yahoo.com>
2024-08-26 15:14:41 -0700

    Update

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,3 +1,5 @@
+import art
+
 def add(n1, n2):
     return n1 + n2
 
@@ -20,7 +22,28 @@ operations = {
 
 # print(operations["*"](4, 8))
 
-num1 = input("What is the first number?: ")
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation: ")
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate == True:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number? "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == 'y':
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
