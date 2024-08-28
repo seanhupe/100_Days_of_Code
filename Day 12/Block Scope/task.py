7e72a66e923f643d2c382b62dfc666b76b0e6e39
Sean Hupe <seanhupe@yahoo.com>
2024-08-28 11:27:32 -0700

    Update

---
+++ Day 12/Block Scope/task.py
@@ -0,0 +1,4 @@
+# There is no Block Scope in Python
+
+if 3 > 2:
+    a_variable = 10
---
+++ Day 12/Global Constants/task.py
@@ -0,0 +1,11 @@
+# Global Constants  - - UPPER CASE
+
+PI = 3.14159
+
+GOOGLE_URL = "https://www.google.com"
+
+
+def my_func():
+    print(GOOGLE_URL)
+
+my_func()
