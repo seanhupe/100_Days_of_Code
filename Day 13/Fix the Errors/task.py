8cd1497d647fc01d4b2940184ffee036deddfab4
Sean Hupe <seanhupe@yahoo.com>
2024-08-29 14:16:32 -0700

    Update

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,7 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you? "))
+except ValueError:
+    print("You have typed in an invalid number. Please use numerical numbers.")
+    age = int(input("How old are you? "))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
