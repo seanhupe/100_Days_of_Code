575c705b9a63975629d8e81b08b4304136c829c0
Sean Hupe <seanhupe@yahoo.com>
2024-08-28 17:40:33 -0700

    Update

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -5,12 +5,16 @@ print(logo)
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
+
 # Function to check user's guess against actual answer
-def check_answer(user_guess, actual_answer):
+def check_answer(user_guess, actual_answer, turns):
+    """Checks answer against guess, returns the number of turns remaining"""
     if user_guess > actual_answer:
         print("Too high.")
+        return turns - 1
     elif user_guess < actual_answer:
         print("Too Low.")
+        return turns - 1
     else:
         print(f"You got it! The answer was {actual_answer}")
 
@@ -23,22 +27,32 @@ def set_difficulty():
         return HARD_LEVEL_TURNS
 
 
+def game():
+    # Choose a random number 1 - 100
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+    answer = randint(1, 100)
+    print(f"Pssst, the correct answer is: {answer}")
 
-# Choose a random number 1 - 100
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100")
-answer = randint(1, 100)
-print(f"Pssst, the correct answer is{answer}")
-
-
-
-# Let the user guess a number
-int(input("Make a guess: "))
-turns = set_difficulty()
-print(f"You have {turns} attempts remaining to guess the number.")
+    turns = set_difficulty()
 
 
+    # Repeat the guessing functionality if they get it wrong
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        # Let the user guess a number
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses - You Lose!")
+            return
+        elif guess != answer:
+            print("Guess again.")
 
-# Track number of turns/guesses and reduce by 1 if they get it wrong
+    # Track number of turns/guesses and reduce by 1 if they get it wrong
 
-# Repeat the guessing functionality if they get it wrong
+
+game()
+
+
