3f211a26907990d0845ff7a698e6a06fc0c039b2
Sean Hupe <seanhupe@yahoo.com>
2024-08-29 14:23:43 -0700

    Update

--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,8 @@
-word_per_page = 0
+# word_per_page = 0
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
+word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
+
+print(f"Pages = {pages}")
+print(f"Words Per Page = {word_per_page}")
 print(total_words)
