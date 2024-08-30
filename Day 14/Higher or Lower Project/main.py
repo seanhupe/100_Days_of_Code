bfe604aed1f683e3c4a06a922cdba2b27357abc2
Sean Hupe <seanhupe@yahoo.com>
2024-08-30 16:56:09 -0700

    Update

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -24,36 +24,40 @@ def check_answer(user_guess, a_followers, b_followers):
 
 print(logo)
 score = 0
+game_should_continue = True
 
-# Generate a random account from the game data file
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:                      # in case they are the same
+
+while game_should_continue:
+    # Generate a random account from the game data file
+    account_a = random.choice(data)
     account_b = random.choice(data)
+    if account_a == account_b:                      # in case they are the same
+        account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print(vs)                                       # Pring the VS ASC art
-print(f"Against B: {format_data(account_b)}.")
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)                                       # Pring the VS ASC art
+    print(f"Against B: {format_data(account_b)}.")
 
-# Ask user for a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    # Ask user for a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# Check if user is correct
-## Get follower count of each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    # Check if user is correct
+    ## Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
 
-## Use if statement to check if user is correct
+    ## Use if statement to check if user is correct
 
-# Give user feedback on their guess
-# Score Keeping
-if is_correct:
-    score += 1
-    print(f"You're right! Current score {score}")
-else:
-    print(f"Sorry, that is wrong. Final Score {score}")
+    # Give user feedback on their guess
+    # Score Keeping
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score {score}")
+    else:
+        print(f"Sorry, that is wrong. Final Score {score}")
+        game_should_continue = False
 
 
 
