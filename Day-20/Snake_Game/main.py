80c5b6dc5f913e8f9158dc8e52c025e1dba8fb00
Sean Hupe <seanhupe@yahoo.com>
2024-09-17 13:31:23 -0700

    updates

--- Day-20/Snake_Game/main.py
+++ Day-20/Snake_Game/main.py
@@ -4,6 +4,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game!")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -15,6 +16,8 @@ for position in starting_positions:
     new_segment.penup()
     new_segment.goto(position)
     segments.append(new_segment)
+
+screen.update()
 
 game_is_on = True
 while game_is_on:
