1129b1c42cacdd3621a084ce2061191f2e6f9b67
Sean Hupe <seanhupe@yahoo.com>
2024-09-17 12:45:28 -0700

    updates

---
+++ Day-20/Snake_Game/main.py
@@ -0,0 +1,22 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Snake Game!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
