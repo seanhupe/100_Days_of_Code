755e8fbc85cdbdbdc39c2eae7ff352feef7efdbe
Sean Hupe <seanhupe@yahoo.com>
2024-09-16 19:38:03 -0700

    Updates

--- Day-19/main.py
+++ Day-19/main.py
@@ -27,6 +27,14 @@ if user_bet:
 while is_race_on:
 
     for turtle in all_turtles:
+        if turtle.xcor() > 223:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You Win! The {winning_color} turtle is the winner!")
+            else:
+                print(f"LOSER! The {winning_color} turtle is the winner!")
+
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
 
