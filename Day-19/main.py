afaffa80e379dc1181baf6ba76cfa6a0319f13c2
Sean Hupe <seanhupe@yahoo.com>
2024-09-16 19:26:08 -0700

    Updates

--- Day-19/main.py
+++ Day-19/main.py
@@ -1,28 +1,33 @@
+import turtle
 from tarfile import TruncatedHeaderError
 from turtle import Turtle, Screen
 import random
 
 is_race_on = False
 
-tim = Turtle()
+new_turtle = Turtle()
 screen = Screen()
 screen.setup(500, 400)
 user_bet = screen.textinput("Make your bet", "Which turtle will win?   Enter a color:  ")
 colors = ["red", "orange", "SlateGray4", "green", "RoyalBlue", "purple"]
 
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 for turtle_index in range(0, 6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(all_turtles)
+
 
 if user_bet:
     is_race_on = True
 
 while is_race_on:
-    random.randint(0, 10)
+    random_distance = random.randint(0, 10)
+    turtle.forward((random_distance))
 
 
 
