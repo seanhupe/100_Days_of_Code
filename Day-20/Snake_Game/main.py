a5177492f13c74add72ee69004fbba891b1d7439
Sean Hupe <seanhupe@yahoo.com>
2024-09-17 12:52:07 -0700

    updates

--- Day-20/Snake_Game/main.py
+++ Day-20/Snake_Game/main.py
@@ -5,7 +5,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game!")
 
+segment_1 = Turtle(shape="square")
+segment_1.color("white")
 
+segment_2 = Turtle(shape="square")
+segment_2.color("white")
+segment_2.goto(x=-20, y=0)
+
+segment_3 = Turtle(shape="square")
+segment_3.color("white")
+segment_3.goto(x=-40, y=0)
 
 
 
