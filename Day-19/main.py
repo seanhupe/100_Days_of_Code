6767ae69a90c8ce255c22658a089af22e30a380a
Sean Hupe <seanhupe@yahoo.com>
2024-09-16 19:05:06 -0700

    Updates

--- Day-19/main.py
+++ Day-19/main.py
@@ -4,9 +4,10 @@ tim = Turtle()
 screen = Screen()
 screen.setup(500, 400)
 user_bet = screen.textinput("Make your bet", "Which turtle will win?   Enter a color:  ")
-print(user_bet)
+
 
 tim = Turtle()
+tim.penup()
 tim.goto(x=-230, y=-100)
 
 screen.exitonclick()
