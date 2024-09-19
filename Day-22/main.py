1ec49b81c2eb8c39b7e064f6fbb3c974d1e8ab96
Sean Hupe <seanhupe@yahoo.com>
2024-09-19 13:58:35 -0700

    Updates

--- Day-22/main.py
+++ Day-22/main.py
@@ -1,5 +1,5 @@
 from turtle import Screen, Turtle
-import time
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,21 +7,10 @@ screen.bgcolor("black")
 screen.title("The Pong Game ASSHOLE!!")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 
 screen.listen()
