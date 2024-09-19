7effe2513742dfbb18e52f4da6c288c262829ca6
Sean Hupe <seanhupe@yahoo.com>
2024-09-19 13:18:26 -0700

    Updates

---
+++ Day-22/main.py
@@ -0,0 +1,14 @@
+from turtle import Screen
+# from snake import Snake
+# from food import Food
+# from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("The Pong Game ASSHOLE!!")
+screen.tracer(0)
+
+
+screen.exitonclick()
