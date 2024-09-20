264e7bed1c64ccf71497954250f0481d996a55d9
Sean Hupe <seanhupe@yahoo.com>
2024-09-20 15:11:28 -0700

    Initial push

--- Day-22 Turtle Crossing Game/main.py
+++ Day-22 Turtle Crossing Game/main.py
@@ -22,3 +22,13 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    #Detect Collisions of Turtle w/Cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+
+
+screen.exitonclick()
+
