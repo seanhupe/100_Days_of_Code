560da2aff03acdedd536a2e8c48382c48b838b52
Sean Hupe <seanhupe@yahoo.com>
2024-09-23 15:33:46 -0700

    Updates

--- Day-24/main.py
+++ Day-24/main.py
@@ -1,4 +1,5 @@
 # file = open("my_file.txt")
+## - When locating a file with absolute path on the Desktop
 # with open("C:/Users/seanh/Desktop/my_file.txt") as file:
 with open("my_file.txt") as file:
     contents = file.read()
