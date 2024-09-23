6706f27afc500b45462dc68a244b82ce06827be3
Sean Hupe <seanhupe@yahoo.com>
2024-09-23 14:35:00 -0700

    Updates

--- Day-24/main.py
+++ Day-24/main.py
@@ -1,8 +1,8 @@
 # file = open("my_file.txt")
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
 
-
-
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew Text.")
 
