7b35edb8bd4c7105977fbd083f8e0e1b40283500
Sean Hupe <seanhupe@yahoo.com>
2024-09-23 14:13:28 -0700

    updates

--- Day-20/Snake_Game/main.py
+++ Day-20/Snake_Game/main.py
@@ -34,14 +34,16 @@ while game_is_on:
 
     #Detect collision with wall
     if snake.head.xcor() > 283 or snake.head.xcor() < -283 or snake.head.ycor() > 283 or snake.head.ycor() < -283:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
 
     #Detect collision with tail - if head collides with any segment of the tail
         # trigger game_over
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
 
 screen.exitonclick()
--- Day-20/Snake_Game/scoreboard.py
+++ Day-20/Snake_Game/scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Ariel", 24, "normal")
+FONT = ("Ariel", 22, "normal")
 
 class Scoreboard(Turtle):
 
@@ -16,7 +16,7 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}   High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
