0b0f1822ec5055a04ed08188e732bbd5ec00bb55
Sean Hupe <seanhupe@yahoo.com>
2024-09-24 14:37:52 -0700

    Updates

--- day-25/main.py
+++ day-25/main.py
@@ -2,13 +2,18 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if (row[1]) != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if (row[1]) != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+import pandas
 
+data = pandas.read_csv("weather_data.csv")
+print(type(data))
+# print(data["temp"])
