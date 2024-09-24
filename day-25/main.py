ac50637286fa678b3910e2dffff32ef329ab0b04
Sean Hupe <seanhupe@yahoo.com>
2024-09-24 15:39:29 -0700

    Updates

--- day-25/main.py
+++ day-25/main.py
@@ -29,8 +29,35 @@ temp_average = sum(temp_list) / len(temp_list)
 print(round(temp_average, 2))
     # Or...using Pandas
 print(data["temp"].mean())
+
+# Find the Maxium Temp
 print(data["temp"].max())
 
-# Get Data in Columns
+# Get Data in Columns*
 print(data["condition"])
+print(data.condition)
+print(data["day"])
+print(data.day)
 
+# Get Data in an individual Row
+print(data[data.day == "Monday"])
+max_row = (data[data.day == "Monday"]).max()
+
+# Get the Row that has the Maximum Temperature
+print(data[data.temp == data.temp.max()])
+
+# Get Monday's temp, but convert to Fahrenheit
+monday = data[data.day == "Monday"]
+monday_temp = monday.temp[0]
+monday_temp_F = monday_temp * 9/5 + 32
+print(monday_temp_F)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+dataframe_from_scratch = pandas.DataFrame(data_dict)
+print(dataframe_from_scratch)
+dataframe_from_scratch.to_csv("new_data.csv")
