abb28913ffb969182f725ec417a8e36512f768b2
Sean Hupe <seanhupe@yahoo.com>
2024-09-24 15:06:57 -0700

    Updates

--- day-25/main.py
+++ day-25/main.py
@@ -15,5 +15,22 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(type(data))
+# print(type(data["temp"]))
 # print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# Calculate the Average Temp
+temp_average = sum(temp_list) / len(temp_list)
+print(round(temp_average, 2))
+    # Or...using Pandas
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# Get Data in Columns
+print(data["condition"])
+
