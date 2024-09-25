869b6cb9eac4d28251561e7ba3b7ccfee17eb358
Sean Hupe <seanhupe@yahoo.com>
2024-09-25 16:26:45 -0700

    Updates

--- day-26 NATO Alphabet Project/main.py
+++ day-26 NATO Alphabet Project/main.py
@@ -23,5 +23,16 @@ for (index, row) in student_data_frame.iterrows():
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(data.to_dict())
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+# print(phonetic_dict)
+
+
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+word = input("Enter a word: ").upper()
+#[new_item for item in list]
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
