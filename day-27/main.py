e1c17ebb72f00337ab2146472a121fc1b5a4a319
Sean Hupe <seanhupe@yahoo.com>
2024-09-26 17:49:07 -0700

    Updates

--- day-27/main.py
+++ day-27/main.py
@@ -7,13 +7,17 @@ window.minsize(width=700, height=500)
 
 ## Label
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.pack(side="left")
 #This packs the label on the screen and has different options
 # my_label.pack(side="left")
 # my_label.pack(side="bottom")
 # my_label.pack(side="top")
 # my_label.pack(side="right")
 # my_label.pack(expand=True)
+
+import turtle
+time = turtle.Turtle()
+
 
 
 window.mainloop()
