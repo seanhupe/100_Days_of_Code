205986f390faebb8e6a7eeeeccd5696ded2c14dc
Sean Hupe <seanhupe@yahoo.com>
2024-09-30 20:16:56 -0700

    Updates

--- day-28 Pomodoro Start/main.py
+++ day-28 Pomodoro Start/main.py
@@ -1,3 +1,6 @@
+from tkinter import *
+from turtledemo.penrose import start
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,6 +16,57 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(300)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Project")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+
+## Background tomato image
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+
+
+##LABEL - TIMER
+timer = Label(text="TIMER", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45, "bold"))
+timer.grid(column=1, row=0)
+
+
+# LABEL CHECKMARK
+check_marks = Label(text="✔️", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
+## BUTTONS
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+window.mainloop()
+
+
+
+
+
+
