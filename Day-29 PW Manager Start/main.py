c348439eb472ec8fd22161004dd1b2b953b3f3b5
Sean Hupe <seanhupe@yahoo.com>
2024-10-04 16:53:51 -0700

    Updates

--- Day-29 PW Manager Start/main.py
+++ Day-29 PW Manager Start/main.py
@@ -1,5 +1,49 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
+# ---------------------------- UI SETUP ------------------------------- #+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+## Background tomato image
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+
+## LABEL - Website
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+## LABEL - Email/Username
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+## LABEL - Password
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+## ENTRIES
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+## BUTTONS
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+#
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
+
+window.mainloop()
