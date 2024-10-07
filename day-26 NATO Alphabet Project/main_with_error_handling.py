48b8309794f2a2d1b9485b16d8560f8b2ccbef7b
Sean Hupe <seanhupe@yahoo.com>
2024-10-07 10:46:21 -0700

    Updates

--- day-26 NATO Alphabet Project/main_with_error_handling.py
+++ day-26 NATO Alphabet Project/main_with_error_handling.py
@@ -4,8 +4,12 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # print(data.to_dict())
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+# print(phonetic_dict)
 
 word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+try:
+    output_list = [phonetic_dict[letter] for letter in word]
+except KeyError:
+    print("Sorry, only letters in the alphabet.")
+else:
+    print(output_list)
