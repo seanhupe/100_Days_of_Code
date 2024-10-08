e60a93299183bd6a8b952610053fc4f8864a2023
Sean Hupe <seanhupe@yahoo.com>
2024-10-08 10:10:39 -0700

    Updates

--- day-31_Flash_Card_Project/main.py
+++ day-31_Flash_Card_Project/main.py
@@ -6,7 +6,14 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
 
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0)
 
 
 
@@ -14,6 +21,6 @@ window.mainloop()
 
 
 
-my_image = PhotoImage(file="path/to/image_file.png")
-button = Button(image=my_image, highlightthickness=0)
+# my_image = PhotoImage(file="path/to/image_file.png")
+# button = Button(image=my_image, highlightthickness=0)
 
