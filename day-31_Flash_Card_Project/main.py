c6fecf9102bc72cf24fe2e1f1682f69a7278e920
Sean Hupe <seanhupe@yahoo.com>
2024-10-08 10:49:17 -0700

    Updates

--- day-31_Flash_Card_Project/main.py
+++ day-31_Flash_Card_Project/main.py
@@ -11,10 +11,13 @@ current_card = {}
 
 def next_card():
     global current_card
+    global flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
@@ -26,7 +29,7 @@ def flip_card():
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
