603dd01d7f723c3d7a5c230e768c37c15d69a1b3
Sean Hupe <seanhupe@yahoo.com>
2024-10-14 15:14:58 -0700

    updates

--- day-35-Rain_Alert_API/main.py
+++ day-35-Rain_Alert_API/main.py
@@ -1,13 +1,17 @@
 import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "691011b732213b48da6c0d9657639d2d"
-lat =
-lon =
 
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=api_key
+weather_params = {
+    "lat": 36.162663,
+    "lon": -86.781601,
+    "appid": api_key,
+}
 
 
-response = requests.get("https://opentdb.com/api.php", params=parameters)
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.status_code)
 response.raise_for_status()
 data = response.json()
 question_data = (data["results"])
