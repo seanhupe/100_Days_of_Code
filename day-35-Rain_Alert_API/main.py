aee7964ae1155eb1144ed1599a46a15d63784e54
Sean Hupe <seanhupe@yahoo.com>
2024-10-14 15:47:29 -0700

    updates

--- day-35-Rain_Alert_API/main.py
+++ day-35-Rain_Alert_API/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "691011b732213b48da6c0d9657639d2d"
@@ -7,11 +8,30 @@ weather_params = {
     "lat": 36.162663,
     "lon": -86.781601,
     "appid": api_key,
+    "cnt": 4,
 }
 
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code)
 response.raise_for_status()
-data = response.json()
-question_data = (data["results"])
+print(response.status_code)
+print(response.json())
+
+weather_data = response.json()
+
+# question_data = (data["results"])
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        # print("Bring an umbrella!")
+        will_rain = True
+
+if will_rain:
+    print("Bring Umbrella")
+else:
+    print("No Umbrella needed.")
+
+
+# if code <= sunset or time_now <= sunrise:
