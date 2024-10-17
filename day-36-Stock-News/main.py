9d7af0724dda1ccaa3f1a4a5c35b681f6540af71
Sean Hupe <seanhupe@yahoo.com>
2024-10-16 18:23:47 -0700

    updates

--- day-36-Stock-News/main.py
+++ day-36-Stock-News/main.py
@@ -19,8 +19,15 @@ stock_params = {
 }
 
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
-print(response.json())
+# print(response.json())
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
 #TODO 2. - Get the day before yesterday's closing stock price
+
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 
