fb9390a681f236374b5932463568332d30befbbf
Sean Hupe <seanhupe@yahoo.com>
2024-10-17 19:19:54 -0700

    updates

--- day-36-Stock-News/main.py
+++ day-36-Stock-News/main.py
@@ -60,7 +60,7 @@ if abs(diff_percent) > 0:
     articles = news_response.json()["articles"]
     print(articles)
 
-    ## STEP 2: https://newsapi.org/ 
+    ## STEP 2: https://newsapi.org/    ,
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
