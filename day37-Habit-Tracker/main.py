3316e798fcb546e008f1dce9a7157a42a2f763b8
Sean Hupe <seanhupe@yahoo.com>
2024-10-29 16:47:04 -0700

    Updates

--- day37-Habit-Tracker/main.py
+++ day37-Habit-Tracker/main.py
@@ -56,8 +56,8 @@ new_pixel_data = {
     "quantity": "4.5",
 }
 
-response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
-print(response.text)
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
 
 # -------------------------------------------------------------------------
 ## DELETE
