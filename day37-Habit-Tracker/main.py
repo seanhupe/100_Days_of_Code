81c163d996b4a36707aa510b83d06e42cb7b8f41
Sean Hupe <seanhupe@yahoo.com>
2024-10-29 16:35:44 -0700

    Updates

--- day37-Habit-Tracker/main.py
+++ day37-Habit-Tracker/main.py
@@ -1,7 +1,9 @@
 import requests
+from datetime import datetime
 
 USERNAME = "seanhupe"
 TOKEN = "asaldkfjghakdjf"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -14,7 +16,7 @@ uers_params = {
 
 # response = requests.post(url=pixela_endpoint, json=uers_params)
 # print(response.text)
-
+# -------------------------------------------------------------------------
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
@@ -29,5 +31,18 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+# -------------------------------------------------------------------------
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "9.74",
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
