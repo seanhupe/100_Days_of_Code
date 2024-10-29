da51011bed69affe2c09b2bcdcaa318435afb93d
Sean Hupe <seanhupe@yahoo.com>
2024-10-29 15:55:20 -0700

    Updates

---
+++ day37-Habit-Tracker/main.py
@@ -0,0 +1,28 @@
+import requests
+
+USERNAME = "seanhupe"
+TOKEN = "asaldkfjghakdjf"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+uers_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=uers_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "m",
+    "type": "float",
+    "color": "ajisai",
+}
+response = requests.post(url=graph_endpoint, json=graph_config)
+print(response.text)
